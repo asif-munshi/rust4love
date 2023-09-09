f2c5ec217a39fc54568cfb6651c87120014efa41
asif-munshi <asifmunshe2978@gmail.com>
2023-09-09 22:25:04 +0600

    numeric_operations

---
+++ numeric_operations/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    // addition
+    let sum = 5 + 10;
+
+    // subtraction
+    let difference = 95.5 - 4.3;
+
+    // multiplication
+    let product = 4 * 30;
+
+    // division
+    let quotient = 56.7 / 32.2;
+    let truncated = -5 / 3; // Results in -1
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("Addition: {sum}");
+    println!("Subtraction: {difference}");
+    println!("Multiplication: {product}");
+
+    println!("Division: {quotient}");
+    println!("Division (Negative): {truncated}");
+    println!("Remainder: {remainder}");
+}
